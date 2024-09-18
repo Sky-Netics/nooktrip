22f08e4cf39c036168485cb8318b8722baa0818b
farshad-nejati <nejati.farshad93@gmail.com>
2024-09-18 19:23:10 +0330

    * feat(Header.tsx): add link to home page in logo image

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,19 +4,22 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between items-center">
-      <Image
-        className="w-20 h-12 md:w-24 md:h-16 object-cover"
-        src="/Logo.png"
-        priority
-        width={588}
-        height={348}
-        alt="Logo"
-      />
-
+      <Link href="/">
+        <Image
+          className="w-20 h-12 md:w-24 md:h-16 object-cover"
+          src="/Logo.png"
+          priority
+          width={588}
+          height={348}
+          alt="Logo"
+        />
+      </Link>
       <nav>
         <ul className="flex">
           <li>
-            <Link href="/" className="text-amber-500 font-bold">Home</Link>
+            <Link href="/" className="text-amber-500 font-bold">
+              Home
+            </Link>
           </li>
         </ul>
       </nav>
