92b6a742aa63d9f44fcc7060c1b779bff718cdd2
farshad-nejati <nejati.farshad93@gmail.com>
2024-10-27 16:09:48 +0330

    remove useless codes

--- src/app/explore/loading.tsx
+++ src/app/explore/loading.tsx
@@ -1,27 +1,6 @@
 import LoadingLottie from "@/components/LoadingLottie";
-// import { Skeleton } from "@/components/ui/skeleton";
-// import { useState, useEffect } from "react";
-// import { useRouter } from "next/compat/router";
 
 function Loading() {
-  // const [loading, setLoading] = useState(false);
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const handleRouteStart = () => setLoading(true);
-  //   const handleRouteComplete = () => setLoading(false);
-
-  //   router?.events.on('routeChangeStart', handleRouteStart);
-  //   router?.events.on('routeChangeComplete', handleRouteComplete);
-  //   router?.events.on('routeChangeError', handleRouteComplete);
-
-  //   return () => {
-  //     router?.events.off('routeChangeStart', handleRouteStart);
-  //     router?.events.off('routeChangeComplete', handleRouteComplete);
-  //     router?.events.off('routeChangeError', handleRouteComplete);
-  //   };
-  // }, [router]);
-
   return (
     <LoadingLottie />
 
